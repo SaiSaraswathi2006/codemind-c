a2d8c2abf6f7af40c2601782ab54ab07d7b32908
SaiSaraswathi2006 <147473340+SaiSaraswathi2006@users.noreply.github.com>
2024-03-01 20:23:27 +0530

    #include<stdio.h> int main() {     int n,i,a[100];     scanf("%d",&n);     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     for(i=0;i<n;i++){         if(a[0]<a[i]){             a[0]=a[i];         }     }     printf("%d",a[0]); }

---
+++ Maximum_of_an_Array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        if(a[0]<a[i]){
+            a[0]=a[i];
+        }
+    }
+    printf("%d",a[0]);
+}
